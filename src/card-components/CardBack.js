a408afb132f3a0c7f84a2ca60edd5097cb6d3a66
Johnson Joseph <jhonsonj88@gmail.com>
2019-10-06 06:19:49 +0000

    Automatically backed up by Learn

--- src/card-components/CardBack.js
+++ src/card-components/CardBack.js
@@ -12,7 +12,7 @@ export default class CardBack extends Component {
 generateRatingElement = () => {
     // implement meeeee! See the readme for instructions
     if (this.props.IMDBRating !== null){
-      return <img src={this.props.imgMapper} alt="" />;
+      return <img src={imgMapper[this.props.IMDBRating]} alt="" />;
     }
       return <h4>No Rating Found</h4>;
   };
