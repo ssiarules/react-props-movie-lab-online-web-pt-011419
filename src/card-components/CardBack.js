48038f05d5b918eb254cf7a3fae76b041a3a0de1
Johnson Joseph <jhonsonj88@gmail.com>
2019-10-06 06:01:13 +0000

    Automatically backed up by Learn

--- src/card-components/CardBack.js
+++ src/card-components/CardBack.js
@@ -6,7 +6,7 @@ import three from '../assets/stars/3-stars.png'
 import four from '../assets/stars/4-stars.png'
 import five from '../assets/stars/5-stars.png'
 
-const imgMapper = {0: zero, 1: one, 2: two, 3: three, 4: four, 5: five}
+const imgMapper = {0: zero, 1: one, 2: two, 3: three, 4: four, 5: five};
 
 export default class CardBack extends Component {
 
